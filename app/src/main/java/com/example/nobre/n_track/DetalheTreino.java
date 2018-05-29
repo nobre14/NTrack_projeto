0f30da9f18971dfe9d6e36958843f2f167c2db72
Rafael Nobre <rafanobre14@hotmail.com>
2018-05-29 00:04:48 -0300

    Resta construcao dos fragments e termino do treino dao

---
+++ app/src/main/java/com/example/nobre/n_track/DetalheTreino.java
@@ -0,0 +1,13 @@
+package com.example.nobre.n_track;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalheTreino extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe_treino);
+    }
+}
---
+++ app/src/main/java/com/example/nobre/n_track/ListaTreinoFragment.java
@@ -0,0 +1,25 @@
+package com.example.nobre.n_track;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListaTreinoFragment extends Fragment {
+
+
+    public ListaTreinoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_lista_treino, container, false);
+        return view;
+    }
+
+}
